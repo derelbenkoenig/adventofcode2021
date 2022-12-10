63561a1ac38e9ae5901a501d0395fef92f4ee559
Robert Benson <rbenson@bandwidth.com>
2022-12-10 04:00:56 -0500

    2022 day 3 solution

--- 2022/src/Aoc.hs
+++ 2022/src/Aoc.hs
@@ -7,9 +7,11 @@ import Solutions
 
 import qualified Day1
 import qualified Day2
+import qualified Day3
 
 runSolution :: ProblemNumber -> FilePath -> IO ()
 runSolution (ProblemNumber d p) = case d of
     1 -> Day1.runSolution p
     2 -> Day2.runSolution p
+    3 -> Day3.runSolution p
     _ -> const $ fail $ "no solution for day " <> show d <> " problem " <> show p
---
+++ 2022/src/Day3.hs
@@ -0,0 +1,58 @@
+module Day3 where
+
+import Control.Applicative hiding (many, some)
+import Control.Monad (unless, (<=<), replicateM)
+import Data.Char
+import Data.List (foldl1')
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Void
+import Solutions
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+runSolution 1 fp = do
+    input <- T.readFile fp
+    rucksacks <- parseOrFail parseRucksacks fp input
+    priorities <- mapM ((priority <=< commonElement) . compartments) rucksacks
+    print $ sum priorities
+
+runSolution 2 fp = do
+    input <- T.readFile fp
+    groups <- parseOrFail parseElfGroups fp input
+    print groups
+    priorities <- mapM ((priority <=< commonElement) . map (textToSet . unRucksack)) groups
+    print $ sum priorities
+
+newtype Rucksack = Rucksack { unRucksack :: T.Text }
+    deriving Show
+
+type Parser = Parsec Void T.Text
+
+parseRucksack :: Parser Rucksack
+parseRucksack = Rucksack <$> takeWhile1P Nothing isAlpha
+
+parseRucksacks :: Parser [Rucksack]
+parseRucksacks = parseRucksack `sepEndBy` eol
+
+parseElfGroup :: Parser [Rucksack]
+parseElfGroup = replicateM 3 $ parseRucksack <* eol
+
+parseElfGroups :: Parser [[Rucksack]]
+parseElfGroups = many parseElfGroup
+
+textToSet = S.fromList . T.unpack
+
+compartments (Rucksack t) = map textToSet [l, r] where
+    (l, r) = T.splitAt (T.length t `div` 2) t
+
+commonElement sets = do
+    let i = foldl1' S.intersection sets
+    unless (S.size i == 1) $ fail $ "not exactly one element in common in sets " <> show sets
+    return $ S.elemAt 0 i
+
+priority c
+    | isLower c = pure $ fromEnum c - fromEnum 'a' + 1
+    | isUpper c = pure $ fromEnum c - fromEnum 'A' + 27
+    | otherwise = fail $ "unexpected non-alpha character " <> [c]
