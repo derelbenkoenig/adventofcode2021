d592d85c7bd68dde6f9185258c7f63c1bbb7946d
Robert Benson <rbenson@bandwidth.com>
2022-12-13 01:04:40 -0500

    Solution for 2022 day 6

--- 2022/src/Aoc.hs
+++ 2022/src/Aoc.hs
@@ -10,6 +10,7 @@ import qualified Day2
 import qualified Day3
 import qualified Day4
 import qualified Day5
+import qualified Day6
 
 runSolution :: ProblemNumber -> FilePath -> IO ()
 runSolution (ProblemNumber d p) = case d of
@@ -18,4 +19,5 @@ runSolution (ProblemNumber d p) = case d of
     3 -> Day3.runSolution p
     4 -> Day4.runSolution p
     5 -> Day5.runSolution p
+    6 -> Day6.runSolution p
     _ -> const $ fail $ "no solution for day " <> show d <> " problem " <> show p
---
+++ 2022/src/Day6.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE TupleSections #-}
+
+module Day6 where
+
+import Control.Applicative hiding (many, some)
+import Control.Monad (mfilter, replicateM, void)
+import Data.Foldable (foldl')
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Day3 (textToSet)
+import Solutions
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+runSolution n fp = do
+    input <- T.readFile fp
+    markerLen <- startMarkerLen n
+    sopOffset <- fmap snd $ parseOrFail (findStartMarker markerLen) fp input
+    print sopOffset
+
+startMarkerLen :: (MonadFail m, Integral i) => i -> m Int
+startMarkerLen 1 = pure sopLength
+startMarkerLen 2 = pure somLength
+startMarkerLen _ = fail "problem must be 1 or 2"
+
+allDistinct :: T.Text -> Bool
+allDistinct t = S.size (textToSet t) == T.length t
+
+sopLength = 4
+somLength = 14
+
+parseStartMarker :: Int -> Parser T.Text
+parseStartMarker n = mfilter (allDistinct) $ takeP (Just $ show n <> " distinct chars") n
+
+findStartMarker :: Int -> Parser (T.Text, Int)
+findStartMarker n = go n where
+    go l = fmap (, l) (try $ parseStartMarker n) <|>
+        void anySingle *> go (l + 1)
+
