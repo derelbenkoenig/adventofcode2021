40ad6a8021f1690d0c6fc0cb6c19eda7325f058f
Robert Benson <derelbenkoenig@gmail.com>
2024-12-20 17:09:31 -0500

    day 3 part 2

--- 2024/src/Day3.hs
+++ 2024/src/Day3.hs
@@ -4,25 +4,47 @@ module Main (main) where
 
 import Parsing
 import qualified Parsing.Lexer as L
-import Control.Applicative hiding (many, some)
 import Data.Foldable (foldl')
 import Data.Char (digitToInt)
 
 main :: IO ()
 main = do
     instructions <- parseOrFail parseInstructions "inputs/day3.txt"
-    print $ sum $ fmap doInstr instructions
+    print $ sum $ fmap performInstr instructions
+    print $ fst $ foldl'
+        (\ !(!total, !doing) instr ->
+            case instr of
+              MulInstr x y -> (if doing then total + x * y else total, doing)
+              DoInstr -> (total, True)
+              DontInstr -> (total, False)
+        )
+        (0, True)
+        instructions
 
-data Instruction = MulInstr !Int !Int
+data Instruction =
+    MulInstr !Int !Int
+      | DoInstr
+      | DontInstr
 
-doInstr :: Instruction -> Int
-doInstr (MulInstr a b) = a * b
+performInstr :: Instruction -> Int
+performInstr (MulInstr a b) = a * b
+performInstr _ = 0
 
 parseInstructions :: Parser [Instruction]
 parseInstructions =
-    manyOptional $ MulInstr <$>
-        (string "mul(" *> oneToThreeDigitNumber) <*>
-        (char ',' *> oneToThreeDigitNumber <* char ')')
+    manyOptional $ try parseMulInstr <|> try parseDoInstr <|> parseDontInstr
+
+parseMulInstr :: Parser Instruction
+parseMulInstr =
+    MulInstr
+    <$> (string "mul(" *> oneToThreeDigitNumber)
+    <*> (char ',' *> oneToThreeDigitNumber <* char ')')
+
+parseDoInstr :: Parser Instruction
+parseDoInstr = DoInstr <$ string "do()"
+
+parseDontInstr :: Parser Instruction
+parseDontInstr = DontInstr <$ string "don't()"
 
 oneToThreeDigitNumber :: Parser Int
 oneToThreeDigitNumber = do
