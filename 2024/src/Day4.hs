0c1f86cf12fb3525c6ab613ea74466879edf7105
Robert Benson <derelbenkoenig@gmail.com>
2024-12-20 21:22:42 -0500

    day 4 part 2

--- 2024/src/Day4.hs
+++ 2024/src/Day4.hs
@@ -14,6 +14,7 @@ main :: IO ()
 main = do
     (wordSearch, _) <- parseOrFailStateful parseWordSearch "inputs/day4.txt" (0,0)
     print $ sum $ extend (findWordAnyDirection "XMAS") wordSearch
+    print $ sum $ extend (fromEnum . findExedMas) wordSearch
 
 type NeighborFinder = Grid Char -> (Int, Int) -> [(Int, Int)]
 
@@ -32,6 +33,22 @@ findWord w g nf = experiment (nf g) g == w
 
 findWordAnyDirection :: String -> Grid Char -> Int
 findWordAnyDirection w g = sum $ fmap (fromEnum . (findWord w g)) neighborFinders
+
+squareNeighborFinder :: NeighborFinder
+squareNeighborFinder g ix = filter (inBounds g) $ do
+    rowMv <- [-1, 0, 1]
+    colMv <- [-1, 0, 1]
+    pure $ bimap (+ rowMv) (+ colMv) ix
+
+isExedMas :: [Char] -> Bool
+isExedMas cs =
+    length cs == 9
+    && cs !! 4 == 'A'
+    && (cs !! 0 == 'M' && cs !! 8 == 'S' || cs !! 0 == 'S' && cs !! 8 == 'M')
+    && (cs !! 2 == 'M' && cs !! 6 == 'S' || cs !! 2 == 'S' && cs !! 6 == 'M')
+
+findExedMas :: Grid Char -> Bool
+findExedMas g = isExedMas $ experiment (squareNeighborFinder g) g
 
 parseWordSearch :: StatefulParser (Int, Int) (Grid Char)
 parseWordSearch = do
