79de30d53ac793656c96a92d274b295b8902bfd0
derelbenkoenig <derelbenkoenig@gmail.com>
2025-01-16 22:29:26 -0500

    day 9 part 1 but wrong (committing before messing with it so I can see what I'm changing better)

---
+++ 2024/src/Day9.hs
@@ -0,0 +1,70 @@
+module Main (main) where
+
+import Control.Monad.ST
+
+import Data.Array.MArray
+import Data.Array.ST
+import Data.Char (digitToInt)
+import Data.Foldable (foldl')
+
+import Parsing
+
+main :: IO ()
+main = do
+    segments <- parseOrFail parseDiskSegments "inputs/day9.txt"
+    let stDisk = diskFromDiskmap segments
+    print $ runST $ stDisk >>= compactDisk >>= diskChecksum
+
+data DiskSegment = FileSeg Int | FreeSeg Int
+    deriving (Eq, Show)
+
+numBlocks :: DiskSegment -> Int
+numBlocks (FileSeg x) = x
+numBlocks (FreeSeg x) = x
+
+parseDiskSegments :: Parser [DiskSegment]
+parseDiskSegments =
+    zipWith ($) (cycle [FileSeg, FreeSeg])
+        <$> (many (digitToInt <$> digitChar) <* eol)
+
+diskFromDiskmap :: [DiskSegment] -> ST s (STUArray s Int Int)
+diskFromDiskmap diskmap = do
+    let totalSize = foldl' ((. numBlocks) . (+)) 0 diskmap
+    diskArr <- newArray (0, totalSize - 1) (-1)
+    let go ix fileId segments =
+            case segments of
+              [] -> pure diskArr
+              (FileSeg n):rest ->
+                  if n <= 0
+                     then go ix (fileId + 1) rest
+                     else do
+                         writeArray diskArr ix fileId
+                         go (ix + 1) fileId (FileSeg (n - 1):rest)
+              (FreeSeg n):rest ->
+                  if n <= 0
+                     then go ix fileId rest
+                     else go (ix + 1) fileId (FreeSeg (n - 1):rest)
+    go 0 0 diskmap
+
+compactDisk :: STUArray s Int Int -> ST s (STUArray s Int Int)
+compactDisk arr = getBounds arr >>= uncurry go
+    where
+        go ixL ixR =
+            if ixR <= ixL
+               then pure arr
+               else do
+                   currFileId <- readArray arr ixL
+                   if currFileId == (-1)
+                      then do
+                          rightFileId <- readArray arr ixR
+                          writeArray arr ixL rightFileId
+                          writeArray arr ixR (-1)
+                          go (ixL + 1) (ixR - 1)
+                      else go (ixL + 1) ixR
+
+diskChecksum :: STUArray s Int Int -> ST s Int
+diskChecksum arr = foldl' foo 0 <$> getAssocs arr where
+    foo acc (ix, fileId) =
+        if fileId >= 0
+           then acc + ix * fileId
+           else acc
